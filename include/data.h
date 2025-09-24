05242479c26212d4d775722ea348b36e9801b65f
Quentin Binstok <quentin.binstok@student.uliege.be>
2025-09-24 09:36:43 +0200

    fix: refactored the code

---
+++ include/data.h
@@ -0,0 +1,25 @@
+#ifndef __DATA__
+#define __DATA__
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define GET(data, i, j) ((data)->values[(data)->nx * (j) + (i)])
+#define SET(data, i, j, val) ((data)->values[(data)->nx * (j) + (i)] = (val))
+
+struct data {
+  const char *name;
+  int nx, ny;
+  double dx, dy, *values;
+};
+
+int init_data(struct data *data, const char *name, int nx, int ny, double dx,
+              double dy, double val);
+void free_data(struct data *data);
+int write_data_vtk(struct data *data, int step, int rank);
+int write_manifest_vtk(const char *name, double dt, int nt, int sampling_rate,
+                       int numranks);
+
+#endif
