655cb788ab49757842c12b7335657978b7e2cbeb
Quentin Binstok <quentin.binstok@proton.me>
2025-12-09 09:34:16 +0100

    omp: fixed scalability scripts

--- scalability/scalability_mpi_strong.sh
+++ scalability/scalability_mpi_strong.sh
@@ -2,30 +2,28 @@
 #SBATCH --job-name="MPI strong scalability"
 #SBATCH --exclusive
 #SBATCH --mem=0
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 
 module load Info0939Tools
 numCpuCores=$SLURM_CPUS_ON_NODE
 startDir=$(pwd)
 executable="/home/ulg/info0939/qbinstok/EM-Wave/hpc_project"
-arg="3"
+args="3 20000"
 
 for binding in close spread; do
     export OMP_PROC_BIND=${binding}
     
     numThreads=1
-    grid_size = 1500
     while [[ $numThreads -le $numCpuCores ]]; do
         export OMP_NUM_THREADS=${numThreads}
 
-        workDir="omp_scalability_weak/omp_${binding}_${numThreads}"
-        output="${numThreads}_threads.log"
+        workDir="omp_scalability_strong"
+        output="${binding}_${numThreads}_threads.log"
 
         mkdir -p ${workDir} && cd ${workDir}
-        ${executable} ${arg} ${grid_size} > ${output}
+        ${executable} ${args} > ${output}
         cd ${startDir}
 
-        (( numThreads *= 4 ))
-        (( grid_size *= 2 ))
+        (( numThreads *= 2 ))
     done
 done
--- scalability/scalability_mpi_weak.sh
+++ scalability/scalability_mpi_weak.sh
@@ -2,28 +2,30 @@
 #SBATCH --job-name="MPI strong scalability"
 #SBATCH --exclusive
 #SBATCH --mem=0
-#SBATCH --time=08:00:00
+#SBATCH --time=02:00:00
 
 module load Info0939Tools
 numCpuCores=$SLURM_CPUS_ON_NODE
 startDir=$(pwd)
 executable="/home/ulg/info0939/qbinstok/EM-Wave/hpc_project"
-args="3 20000"
+arg="3"
 
 for binding in close spread; do
     export OMP_PROC_BIND=${binding}
     
     numThreads=1
+    grid_size=1500
     while [[ $numThreads -le $numCpuCores ]]; do
         export OMP_NUM_THREADS=${numThreads}
 
-        workDir="omp_scalability_strong/omp_${binding}_${numThreads}"
-        output="${numThreads}_threads.log"
+        workDir="omp_scalability_weak"
+        output="${binding}_${numThreads}_threads_${grid_size}_size.log"
 
         mkdir -p ${workDir} && cd ${workDir}
-        ${executable} ${args} > ${output}
+        ${executable} ${arg} ${grid_size} > ${output}
         cd ${startDir}
 
-        (( numThreads *= 2 ))
+        (( numThreads *= 4 ))
+        (( grid_size *= 2 ))
     done
 done
